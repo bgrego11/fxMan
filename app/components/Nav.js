ac906d1873b7b25a3a037505d42db79faf282eb9
StevenS125 <sts12589@gmail.com>
2017-05-06 15:04:17 -0400

    added component and images/background

---
+++ app/components/Nav.js
@@ -0,0 +1,42 @@
+// Include React
+var React = require("react");
+
+// Create the Header component
+// Notice how the header uses React.createClass
+// Notice how it uses a render function which specifies what will be displayed by the component
+var Nav = React.createClass({
+
+  render: function() {
+  
+  	const divStyle = {
+  margin: 10
+};
+
+
+    return (
+
+      <div>
+  		<h1>What would you like to do?</h1>
+  	
+    	<div className="images">
+
+  	<img src="../css/images/calculator.png" alt="Currency Calculator"  width="200" height="200" ClassName="img-rounded" style={divStyle}>
+    </img>
+  	<img src="../css/images/currency.jpg" alt="Current Trades"  width="200" height="200" className="img-rounded" style={divStyle}>
+    </img>
+  	<br></br>
+    <img src="../css/images/news.jpg" alt="Current Currency News"  width="200" height="200" className="img-rounded" style={divStyle}>
+    </img>
+  	<img src="../css/images/forex.jpg" alt="Foreign Exchange rate charts/trends"  width="200" height="200" className="img-rounded" style={divStyle}>
+    </img>
+    </div>
+    </div>
+  	)
+
+  }
+});
+
+module.exports = Nav;
+
+
+// set up create user, add button an function for route and page to create 
